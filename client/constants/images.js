54584caab34b60b451ce1d8552ddefc3d086a4a3
Anurag <anuragma807@gmail.com>
2024-10-16 16:54:03 +0530

    profile screen addded

---
+++ client/constants/images.js
@@ -0,0 +1,19 @@
+const profile = require("../assets/images/ProfileImages/profile.jpg");
+const cover = require("../assets/images/ProfileImages/cover.jpg");
+const photo1 = require("../assets/images/ProfileImages/photo1.jpg");
+const photo2 = require("../assets/images/ProfileImages/photo2.jpg");
+const photo3 = require("../assets/images/ProfileImages/photo3.jpg");
+const photo4 = require("../assets/images/ProfileImages/photo4.jpg");
+const photo5 = require("../assets/images/ProfileImages/photo5.jpg");
+const photo6 = require("../assets/images/ProfileImages/photo6.jpg");
+
+export default {
+    profile,
+    cover,
+    photo1,
+    photo2,
+    photo3,
+    photo4,
+    photo5,
+    photo6
+}
