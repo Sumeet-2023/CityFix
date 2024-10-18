2affc0b9424aac1ed5a01ce07d6d78607938778c
Sumeet Mourya <go69ciq@mytum.de>
2024-10-18 12:23:00 +0200

    folder structure for community, TopTab styling.

---
+++ client/app/(tabs)/home/community/exploreMap.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function ExploreMap() {
+  return (
+    <View>
+      <Text> ExploreMap</Text>
+    </View>
+  )
+}
