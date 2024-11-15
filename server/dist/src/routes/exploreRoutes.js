3464cd65f9f52cba2e5b5dad10040b585cb1b361
MohammedYaseenRon <yaseenron3@gmail.com>
2024-11-15 22:43:53 +0530

    updated

--- server/dist/src/routes/exploreRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const exploreController_1 = require("../controller/exploreController");
-const router = (0, express_1.Router)();
-router.get("/", exploreController_1.getNearbyAll);
-router.get("/clan", exploreController_1.getClanAutocomplete);
-exports.default = router;
--- server/dist/src/routes/notificationRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Notification Routes
-const express_1 = require("express");
-const notificationController_1 = require("../controller/notificationController");
-const router = (0, express_1.Router)();
-router.get('/user/:userId', notificationController_1.getUserNotifications);
-router.patch('/read/:id', notificationController_1.markNotificationAsRead);
-exports.default = router;
